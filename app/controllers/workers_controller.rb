f4755bb79ed6bb5f962a5a228c60fc3778ac35ef
zn <244305150@qq.com>
2022-06-03 22:12:01 +0800

    修改一些内容

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -7,8 +7,6 @@ class WorkersController < ApplicationController
 
    
   def index
-    @worker = Worker.new
-   
     @workers = Worker.all.page( params[:page]).per( Setting.systems.per_page )
    
   end
@@ -27,100 +25,12 @@ class WorkersController < ApplicationController
     redirect_to :action => :index
   end
 
-  def query_all 
-    items = Worker.all
-   
-    obj = []
-    items.each do |item|
-      obj << {
-        #:factory => idencode(factory.id),
-        :id => idencode(item.id),
-       
-        :name => item.name,
-       
-        :idno => item.idno,
-       
-        :phone => item.phone,
-       
-        :gender => item.gender,
-       
-        :state => item.state,
-       
-        :adress => item.adress,
-       
-        :desc => item.desc
-      
-      }
-    end
-    respond_to do |f|
-      f.json{ render :json => obj.to_json}
-    end
-  end
-
-
-
    
   def show
    
     @worker = Worker.find(iddecode(params[:id]))
    
   end
-   
-
-   
-  def new
-    @worker = Worker.new
-    
-  end
-   
-
-   
-  def create
-    @worker = Worker.new(worker_params)
-    face_worker = BaiDuFace.new
-     
-    if @worker.save
-      unless @worker.avatar.file.nil?
-        image = File.join(Rails.root, 'public', @worker.avatar_url)
-        image_base64 = Base64.encode64(File.read(image)).gsub(/\s/, '')
-        @worker.update_attributes!(:avatar_base => image_base64)
-
-        face_worker.add_face_entity(@worker.id)
-      end
-
-      redirect_to :action => :index
-    else
-      render :new
-    end
-  end
-   
-
-   
-  def edit
-   
-    @worker = Worker.find(iddecode(params[:id]))
-   
-  end
-   
-
-   
-  def update
-   
-    @worker = Worker.find(iddecode(params[:id]))
-   
-    if @worker.update(worker_params)
-      unless @worker.avatar.file.nil?
-        image = File.join(Rails.root, 'public', @worker.avatar_url)
-        image_base64 = Base64.encode64(File.read(image)).gsub(/\s/, '')
-        @worker.update_attributes!(:avatar_base => image_base64)
-      end
-      redirect_to edit_worker_path(idencode(@worker.id)) 
-    else
-      render :edit
-    end
-  end
-   
-
    
   def destroy
    
@@ -132,6 +42,62 @@ class WorkersController < ApplicationController
    
 
   
+
+   
+  #def new
+  #  @worker = Worker.new
+  #  
+  #end
+  # 
+
+  # 
+  #def create
+  #  @worker = Worker.new(worker_params)
+  #  face_worker = BaiDuFace.new
+  #   
+  #  if @worker.save
+  #    unless @worker.avatar.file.nil?
+  #      image = File.join(Rails.root, 'public', @worker.avatar_url)
+  #      image_base64 = Base64.encode64(File.read(image)).gsub(/\s/, '')
+  #      @worker.update_attributes!(:avatar_base => image_base64)
+
+  #      face_worker.add_face_entity(@worker.id)
+  #    end
+
+  #    redirect_to :action => :index
+  #  else
+  #    render :new
+  #  end
+  #end
+  # 
+
+  # 
+  #def edit
+  # 
+  #  @worker = Worker.find(iddecode(params[:id]))
+  # 
+  #end
+  # 
+
+  # 
+  #def update
+  # 
+  #  @worker = Worker.find(iddecode(params[:id]))
+  # 
+  #  if @worker.update(worker_params)
+  #    unless @worker.avatar.file.nil?
+  #      image = File.join(Rails.root, 'public', @worker.avatar_url)
+  #      image_base64 = Base64.encode64(File.read(image)).gsub(/\s/, '')
+  #      @worker.update_attributes!(:avatar_base => image_base64)
+  #    end
+  #    redirect_to edit_worker_path(idencode(@worker.id)) 
+  #  else
+  #    render :edit
+  #  end
+  #end
+   
+
+   
 
   
 
--- app/controllers/wx_auths_controller.rb
+++ app/controllers/wx_auths_controller.rb
@@ -1,45 +1,18 @@
-require 'aliyunsdkcore'
-require 'open-uri'
+load "baidu_face.rb"
 
 class WxAuthsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :wxuser_exist?, :only => [:auth_process]
   
-  def image_to_base64(photo)
-    tempfile = open(photo)
-    image_base64 = Base64.encode64(File.read(tempfile)).gsub(/\s/, '')
-    tempfile.close
-    image_base64
-  end
-
-  def baidu_request(url, file)
-    url = url + "?access_token=" + ENV['ACCESS_TOKEN']
-    params = {
-      image: image_to_base64(file),
-      image_type: 'BASE64',
-      group_id_list: Setting.systems.face_group,
-    }
-    sleep(2)
-
-    body = nil
-    RestClient.post(url, params, {content_type: "application/json"}) do |response|
-      body = JSON.parse(response.body)
-    end
-
-    body
-  end
-
   def auth_process
-    url = "https://aip.baidubce.com/rest/2.0/face/v3/search"
-    body = baidu_request(url, params[:photo])
-    puts body 
+    badu_face = BaiDuFace.new
+    body = badu_face.search_face(params[:photo])
     @device = @wxuser.devices.find(params[:equipment])
 
     if body["error_code"] == 0
       users = body['result']['user_list']
       user = users[0]
-      worker_id = user["user_id"]
-      worker = Worker.find_by_number(worker_id)
+      worker = Worker.find_by_number(user["user_id"])
       sign_log = worker.sign_logs.where(:sign_date => Date.today, :device_id => @device.id).first 
       if sign_log.nil?
         @sign_log = SignLog.new(:sign_date => Date.today, :wx_user_id => @wxuser.id, :device_id => @device.id, :worker => worker, :avatar => params[:photo]) 
@@ -62,41 +35,48 @@ class WxAuthsController < ApplicationController
         f.json{ render :json => {:state => "error"}.to_json}
       end
     end
-
-    
-  end
-
-  def aliauth_process 
-    worker = Worker.first
-    imageA = worker.avatar_base
-    imageB = image_to_base64(params[:photo])
-    client = RPCClient.new(
-      access_key_id:     ENV['ACCESS_KEY_ID'],
-      access_key_secret: ENV['ACCESS_KEY_SECRET'],
-      endpoint: 'https://facebody.cn-shanghai.aliyuncs.com',
-      api_version: '2019-12-30'
-    )
-    
-    response = client.request(
-      action: 'CompareFace',
-      params: {
-        #"ImageURLA": "http://viapi-test.oss-cn-shanghai.aliyuncs.com/viapi-3.0domepic/facebody/CompareFace/CompareFace-right1.png",
-        #"ImageURLB": "http://viapi-test.oss-cn-shanghai.aliyuncs.com/viapi-3.0domepic/facebody/CompareFace/CompareFace-left1.png"
-        "ImageDataA": imageA,
-        "ImageDataB": imageB,
-      },
-      opts: {
-        method: 'POST',
-        format_params: true
-      }
-    )
-    
-    puts response
-
-    respond_to do |f|
-      f.json{ render :json => {:state => "success"}.to_json}
-    end
   end
 end
 
+
+#阿里云认证方法暂时不用
+#require 'aliyunsdkcore'
+#require 'open-uri'
+#def aliauth_process 
+#  worker = Worker.first
+#  imageA = worker.avatar_base
+#  imageB = image_to_base64(params[:photo])
+#  client = RPCClient.new(
+#    access_key_id:     ENV['ACCESS_KEY_ID'],
+#    access_key_secret: ENV['ACCESS_KEY_SECRET'],
+#    endpoint: 'https://facebody.cn-shanghai.aliyuncs.com',
+#    api_version: '2019-12-30'
+#  )
+#  
+#  response = client.request(
+#    action: 'CompareFace',
+#    params: {
+#      #"ImageURLA": "http://viapi-test.oss-cn-shanghai.aliyuncs.com/viapi-3.0domepic/facebody/CompareFace/CompareFace-right1.png",
+#      #"ImageURLB": "http://viapi-test.oss-cn-shanghai.aliyuncs.com/viapi-3.0domepic/facebody/CompareFace/CompareFace-left1.png"
+#      "ImageDataA": imageA,
+#      "ImageDataB": imageB,
+#    },
+#    opts: {
+#      method: 'POST',
+#      format_params: true
+#    }
+#  )
+#  
+#  puts response
+#
+#  respond_to do |f|
+#    f.json{ render :json => {:state => "success"}.to_json}
+#  end
+#end
+#def image_to_base64(photo)
+#  tempfile = open(photo)
+#  image_base64 = Base64.encode64(File.read(tempfile)).gsub(/\s/, '')
+#  tempfile.close
+#  image_base64
+#end
 
